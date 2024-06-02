7b7e9dccf7729255cde733915edc42418016276f
Junaid Hasan <spiderss907@gmail.com>
2024-06-02 16:21:31 +0600

    create a navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  RouterProvider
+} from "react-router-dom";
+import { router } from './Router/Route';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className='bg-white'>
-
-    <App />
+    <RouterProvider router={router} />
     </div>
   </React.StrictMode>
 )
