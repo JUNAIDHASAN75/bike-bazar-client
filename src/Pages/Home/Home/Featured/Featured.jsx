14e6fb7d889c761fa9b138a8d0112add50dcaa0e
Junaid Hasan <spiderss907@gmail.com>
2024-06-02 22:17:05 +0600

    modified featued page

--- src/Pages/Home/Home/Featured/Featured.jsx
+++ src/Pages/Home/Home/Featured/Featured.jsx
@@ -4,7 +4,7 @@ import bike1 from "../../../../assets/images/others/feature-bike.png"
 const Featured = () => {
     return (
         <div className="mt-[169px]">
-            <HeaderTitle title={"Fetured Bikes In This Year"} subTitle={"Best bike collection"}></HeaderTitle>
+            <HeaderTitle title={"Featured Bikes In This Year"} subTitle={"Best bike collection"}></HeaderTitle>
             <div className="lg:flex justify-center my-[50px]">
                 <div className="w-full"> <img className="mx-auto" src={bike1} alt="" /></div>
                 <div className="w-full"> <img className="mx-auto" src={bike1} alt="" /></div>
